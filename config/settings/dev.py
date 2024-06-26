a309be4c750ee2d950294c08699e7256f07ec6ad
Ginu <ginujestin@gmail.com>
2024-06-26 12:07:56 +0200

    modified settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,7 +1,11 @@
 from .base import *
+import environs
 
+env = environs.Env()
 
-SECRET_KEY = 
+env.read_env(str(BASE_DIR / '.env'))
+
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -10,7 +14,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str('DB_NAME'),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PWD")
+       # "PORT": env.str("DB_PORT"),
+       # "HOST": env.str("DB_HOST")
     }
 }
