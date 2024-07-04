c3ac0da0d3fb75fe10e69adbba43f71c623c97ba
Ginu <ginujestin@gmail.com>
2024-07-04 13:43:47 +0200

    ORM

--- apps/book/admin.py
+++ apps/book/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, BookAuthor, Tag
 
 admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookAuthor)
+admin.site.register(Tag)
+
 
 # Register your models here.
