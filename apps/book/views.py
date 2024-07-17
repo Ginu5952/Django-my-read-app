57d1d016c87aaa01dd7863dcf7e4317945ba6897
Ginu <ginujestin@gmail.com>
2024-07-17 14:11:50 +0200

    forms

--- apps/book/views.py
+++ apps/book/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from . import models
 from django.http import HttpResponse
+import ipdb
+from .forms import PostBookForm
 
 # Create your views here.
 
@@ -16,5 +18,32 @@ def book_detail(request,isbn):
     return render(request, 'book_detail.html',context)     
 
 
+#def book_post(request):
+    #ipdb.set_trace()
+  #  return render(request,'post.html')    
+
 def book_post(request):
-    return render(request,'post.html')    
+    # get our form in the 2 states
+    # pre-submit - GET
+    # post-submit - POST
+   # ipdb.set_trace()
+    if request.method == 'GET':
+        form = PostBookForm()
+        context = {"form":form}
+        return render(
+            request,
+            'book_post.html',
+            context 
+        )
+    elif request.method == 'POST':
+        data = request.POST
+        form = PostBookForm(data)
+
+        # validation
+        if form.is_valid():
+            # TODO:save to database
+            # TODO:redirect to home page
+            pass
+
+
+
